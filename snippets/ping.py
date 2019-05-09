0e10f0c06f1b50fdc9f000058e75d37251c2b2b7
Sergei <l0kifs91@gmail.com>
2019-05-09 18:00:58 +0600

    change ping procedure

--- snippets/ping.py
+++ snippets/ping.py
@@ -1,10 +1,27 @@
-import os
+import platform
+import subprocess
 
 
-def ping(hostname):
-    response = os.system("ping " + hostname)
-    print(response)
-    if response == 0:
-        return True
+def is_online(host):
+    os_name = platform.system()
+    
+    if os_name == 'Windows':
+        ping_args = ['ping', '-n', '1', '-w', '500', host]
     else:
+        ping_args = ['ping', '-c', '1', '-W', '500', host]
+
+    output = subprocess.Popen(ping_args, stdout=subprocess.PIPE).communicate()[0]
+
+    if output is None:
+        raise Exception('Failed to ping host {}'.format(host))
+
+    output = output.decode('utf-8')
+    if "Destination host unreachable" in output:
+        print(host, "is Offline")
         return False
+    elif "Request timed out" in output:
+        print(host, "is Offline")
+        return False
+    else:
+        print(host, "is Online")
+        return True
