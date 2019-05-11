111d44c59b6576e42814e5ff246ca121cc3a7487
Sergei <l0kifs91@gmail.com>
2019-05-11 22:55:05 +0600

    Update logger.py

--- snippets/logger.py
+++ snippets/logger.py
@@ -5,7 +5,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
+
+# The best type of logging for prod from my point of view
+# https://docs.python.org/3/howto/logging-cookbook.html#using-file-rotation
+# handler = logging.handlers.RotatingFileHandler('log.out', maxBytes=2e+7, backupCount=3)
+
+# Set specific logging level for handler
+# handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
