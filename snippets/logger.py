45946f84ca83ed9088616b742b2bc2adcb940bcb
Sergei <l0kifs91@gmail.com>
2019-05-16 14:56:39 +0600

    Update logger.py

--- snippets/logger.py
+++ snippets/logger.py
@@ -1,19 +1,38 @@
-import logging
+import logging.config
 
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-handler = logging.StreamHandler()
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(funcName)s %(lineno)d - %(message)s'
+        },
+    },
+    'handlers': {
+        'stream_handler': {
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'class': 'logging.StreamHandler',
+        },
+        'rotating_file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'filename': 'errors.log',
+            'maxBytes': 1000,
+            'backupCount': 5,
+            'encoding': 'utf8'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['stream_handler', 'rotating_file_handler'],
+            'level': 'DEBUG'
+        }
+    }
+})
 
-# The best type of logging for prod from my point of view
-# https://docs.python.org/3/howto/logging-cookbook.html#using-file-rotation
-# handler = logging.handlers.RotatingFileHandler('log.out', maxBytes=2e+7, backupCount=3)
-
-# Set specific logging level for handler
-# handler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s - %(lineno)d - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+# inclass_logger_name = self.__class__.__name__
